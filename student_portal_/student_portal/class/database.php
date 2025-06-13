239bd5988f5adcb797a664e2574c10f8a2a430ec
Sukhmn01 <sukhmanpreetdaleke2004@gmail.com>
2025-06-12 22:15:14 -0400

    Add files via upload

---
+++ student_portal_/student_portal/class/database.php
@@ -0,0 +1,17 @@
+<?php
+class Database {
+    private $host = "127.0.0.1";
+    private $username = "root";
+    private $password = "sukhmn12";
+    private $database = "student_records";
+    public $conn;
+
+    public function connect() {
+        $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+        return $this->conn;
+    }
+}
+?>
